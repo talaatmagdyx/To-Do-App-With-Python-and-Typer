352c8a70e9ee2bb36510438071f761c8c9c7a6f4
talaatmagdyx <talaatmagdy75@gmail.com>
2022-12-09 06:58:57 +0200

    Prepare the To-Do Database for Use 1. Set Up the Application’s Configurations 2. Get the To-Do Database Ready for Use 3. Implement the init CLI Command

--- rptodo/cli.py
+++ rptodo/cli.py
@@ -1,12 +1,40 @@
 """This module provides the RP To-Do CLI."""
-
+from pathlib import Path
 from typing import Optional
 
 import typer
 
-from rptodo import __app_name__, __version__
+from rptodo import ERRORS, __app_name__, __version__, config, database
 
 app = typer.Typer()
+
+
+@app.command()
+def init(
+        db_path: str = typer.Option(
+            str(database.DEFAULT_DB_FILE_PATH),
+            "--db-path",
+            "-db",
+            prompt="to-do database location?",
+        ),
+) -> None:
+    """Initialize the to-do database."""
+    app_init_error = config.init_app(db_path)
+    if app_init_error:
+        typer.secho(
+            f'Creating config file failed with "{ERRORS[app_init_error]}"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    db_init_error = database.init_database(Path(db_path))
+    if db_init_error:
+        typer.secho(
+            f'Creating database failed with "{ERRORS[db_init_error]}"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    else:
+        typer.secho(f"The to-do database is {db_path}", fg=typer.colors.GREEN)
 
 
 def _version_callback(value: bool) -> None:
